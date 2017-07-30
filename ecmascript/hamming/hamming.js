3fa5b52a518ce778f92372306f4266630d84f7fe
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-07-30 18:26:02 -0300

    Add ecmascript exercises. Partial commit

---
+++ ecmascript/hamming/hamming.js
@@ -0,0 +1,6 @@
+export default class Hamming {
+  compute = (a, b) => { // Not finished
+    var foo = 0;
+    return foo;
+  }
+}
---
+++ ecmascript/hello-world/hello-world.js
@@ -0,0 +1,5 @@
+export default class HelloWorld {
+  hello() {
+    return 'Hello, World!';
+  }
+}
