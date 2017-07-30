cc9a43d3cb70d78e44942542dc8911c715dbe2b0
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-07-30 20:14:39 -0300

    Refactoring

--- ecmascript/rna-transcription/rna-transcription.js
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -1,22 +1,12 @@
-export default () => ({
-  toRna: (a) => {
-    let rnaComplement = '';
-    for (let i = 0; i < a.length; i++) {
-      let nucleotide = a[i];
-      if (nucleotide !== 'G' && nucleotide !== 'C' && nucleotide !== 'T' && nucleotide !== 'A') {
+const rna = {'G' : 'C', 'C' : 'G', 'T' : 'A', 'A' : 'U'};
+export default class RnaTranscription {
+  toRna(a) {
+    a.split("").map(letter => {
+      if (letter !== 'G' && letter !== 'C' && letter !== 'T' && letter !== 'A') {
         throw new Error('Invalid input DNA.');
       }
-
-      if (nucleotide === 'G')
-        rnaComplement += 'C';
-      if (nucleotide === 'C')
-        rnaComplement += 'G';
-      if (nucleotide === 'T')
-        rnaComplement += 'A';
-      if (nucleotide === 'A')
-        rnaComplement += 'U';
-    }
-
-    return rnaComplement;
+    });
+    
+    return a.split("").map(dna => rna[dna]).join('');
   }
-});
+}
