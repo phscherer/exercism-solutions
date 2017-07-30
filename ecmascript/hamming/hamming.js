c59d561936b07961053ca66e9e386ec6551bea28
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-07-30 19:06:24 -0300

    Add hamming exercise implementation

--- ecmascript/hamming/hamming.js
+++ ecmascript/hamming/hamming.js
@@ -1,6 +1,17 @@
-export default class Hamming {
-  compute = (a, b) => { // Not finished
-    var foo = 0;
-    return foo;
+export default () => ({
+  compute: (a, b) => {
+    if (a.length !== b.length) {
+      throw new Error("DNA strands must be of equal length.");
+    }
+
+    let distance = 0;
+    for (let i = 0; i < a.length; i++) {
+      let x = a[i], y = b[i];
+      if(x !== y) {
+        distance++;
+      }
+    }
+
+    return distance;
   }
-}
+});
