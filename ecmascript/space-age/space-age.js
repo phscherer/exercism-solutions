b37f39a5e1e91bf9ab04921db44c62be5986ce4a
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-08-08 20:13:29 -0300

    Code refactoring

--- ecmascript/space-age/space-age.js
+++ ecmascript/space-age/space-age.js
@@ -1,37 +1,22 @@
 const earthSecondsPerYear = 31557600;
+const timePlanets = {
+  Mercury: 0.2408467,
+  Venus: 0.61519726,
+  Mars: 1.8808158,
+  Jupiter: 11.862615,
+  Saturn: 29.447498,
+  Uranus: 84.016846,
+  Neptune: 164.79132
+};
 
 export default (seconds) => ({
   seconds: seconds,
-
-  onEarth() {
-    return parseFloat((seconds / earthSecondsPerYear).toFixed(2));
-  },
-
-  onMercury() {
-    return parseFloat((seconds / earthSecondsPerYear / 0.2408467).toFixed(2));
-  },
-
-  onVenus() {
-    return parseFloat((seconds / earthSecondsPerYear / 0.61519726).toFixed(2));
-  },
-
-  onMars() {
-    return parseFloat((seconds / earthSecondsPerYear / 1.8808158).toFixed(2));
-  },
-
-  onJupiter() {
-    return parseFloat((seconds / earthSecondsPerYear / 11.862615).toFixed(2));
-  },
-
-  onSaturn() {
-    return parseFloat((seconds / earthSecondsPerYear / 29.447498).toFixed(2));
-  },
-
-  onUranus() {
-    return parseFloat((seconds / earthSecondsPerYear / 84.016846).toFixed(2));
-  },
-
-  onNeptune() {
-    return parseFloat((seconds / earthSecondsPerYear / 164.79132).toFixed(2));
-  }
+  onEarth: () => +(seconds / earthSecondsPerYear).toFixed(2),
+  onMercury: () => +(seconds / earthSecondsPerYear / timePlanets.Mercury).toFixed(2),
+  onVenus: () => +(seconds / earthSecondsPerYear / timePlanets.Venus).toFixed(2),
+  onMars: () => +(seconds / earthSecondsPerYear / timePlanets.Mars).toFixed(2),
+  onJupiter: () => +(seconds / earthSecondsPerYear / timePlanets.Jupiter).toFixed(2),
+  onSaturn: () => +(seconds / earthSecondsPerYear / timePlanets.Saturn).toFixed(2),
+  onUranus: () => +(seconds / earthSecondsPerYear / timePlanets.Uranus).toFixed(2),
+  onNeptune: () => +(seconds / earthSecondsPerYear / timePlanets.Neptune).toFixed(2)
 });
